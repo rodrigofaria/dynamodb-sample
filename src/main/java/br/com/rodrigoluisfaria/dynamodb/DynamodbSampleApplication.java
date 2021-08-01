9eba1e844407d22a3458e2fbbc6e04eb2da7b0ac
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-08-01 13:03:12 -0300

    First code commit

---
+++ src/main/java/br/com/rodrigoluisfaria/dynamodb/DynamodbSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoluisfaria.dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamodbSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoluisfaria/dynamodb/DynamodbSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoluisfaria.dynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
