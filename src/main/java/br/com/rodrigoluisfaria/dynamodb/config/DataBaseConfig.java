8e1543404683be4a4d26079149a1c55bb26e7355
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-08-06 21:29:39 -0300

    Save json data into DynamoDB

--- src/main/java/br/com/rodrigoluisfaria/dynamodb/config/DataBaseConfig.java
+++ src/main/java/br/com/rodrigoluisfaria/dynamodb/config/DataBaseConfig.java
@@ -4,14 +4,23 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.model.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Iterator;
 
 @Component
 @Slf4j
@@ -22,23 +31,14 @@ public class DataBaseConfig {
 
     @PostConstruct
     public void initialize() {
-        DynamoDB dynamoDB = new DynamoDB(createAmazonDynamoDBClient());
-        String tableName = "movie";
+        AmazonDynamoDB dynamoDBClient = createAmazonDynamoDBClient();
+        DynamoDB dynamoDB = new DynamoDB(dynamoDBClient);
 
         try {
-            log.info("Creating table: {}", tableName);
-
-            Table table = dynamoDB.createTable(tableName,
-                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH),
-                            new KeySchemaElement("title", KeyType.RANGE)),
-                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
-                            new AttributeDefinition("title", ScalarAttributeType.S)),
-                    new ProvisionedThroughput(10L, 10L));
-
-            table.waitForActive();
-            log.info("Success. Table status: {}", table.getDescription().getTableStatus());
+            Table table = createTable(dynamoDB);
+            populateTable(table);
         } catch (Exception e) {
-            log.error("Unable to create table: {}", e.getMessage());
+            log.error("Error: {}", e.getMessage());
         }
     }
 
@@ -51,5 +51,46 @@ public class DataBaseConfig {
         return AmazonDynamoDBClientBuilder.standard()
                 .withEndpointConfiguration(endpointConfiguration)
                 .build();
+    }
+
+    private Table createTable(DynamoDB dynamoDB) throws InterruptedException {
+        String tableName = "movie";
+        log.info("Creating table: {}", tableName);
+
+        Table table = dynamoDB.createTable(tableName,
+                Arrays.asList(new KeySchemaElement("year", KeyType.HASH),
+                        new KeySchemaElement("title", KeyType.RANGE)),
+                Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                        new AttributeDefinition("title", ScalarAttributeType.S)),
+                new ProvisionedThroughput(10L, 10L));
+
+        table.waitForActive();
+        log.info("Success. Table status: {}", table.getDescription().getTableStatus());
+        return table;
+    }
+
+    private void populateTable(Table table) throws IOException {
+        log.info("Populating table...");
+
+        InputStream inputStream = this.getClass().getResourceAsStream("/movies/moviedata.json");
+        JsonParser parser = new JsonFactory().createParser(inputStream);
+
+        JsonNode rootNode = new ObjectMapper().readTree(parser);
+        Iterator<JsonNode> iterator = rootNode.iterator();
+
+        ObjectNode currentNode;
+        while (iterator.hasNext()) {
+            currentNode = (ObjectNode) iterator.next();
+
+            int year = currentNode.path("year").asInt();
+            String title = currentNode.path("title").asText();
+
+            table.putItem(
+                    new Item().withPrimaryKey("year", year, "title", title)
+                            .withJSON("info", currentNode.path("info").toString()));
+        }
+
+        log.info("Table populated successfully!");
+        parser.close();
     }
 }
