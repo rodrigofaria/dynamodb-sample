eb57c2d3cffec1a22c77c6c9f1287843b74e8d32
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-08-15 07:26:40 -0300

    Mapped fields in DTO

--- src/main/java/br/com/rodrigoluisfaria/dynamodb/dto/MovieInformationDTO.java
+++ src/main/java/br/com/rodrigoluisfaria/dynamodb/dto/MovieInformationDTO.java
@@ -1,5 +1,6 @@
 package br.com.rodrigoluisfaria.dynamodb.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.List;
@@ -7,11 +8,14 @@ import java.util.List;
 @Data
 public class MovieInformationDTO {
 
+    @JsonProperty("release_date")
     private String releaseDate;
     private Double rating;
+    @JsonProperty("image_url")
     private String imageUrl;
     private String plot;
     private Integer rank;
+    @JsonProperty("running_time_secs")
     private Integer runningTimeSecs;
     private List<String> genres;
     private List<String> directors;
